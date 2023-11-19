1bd8a32b67c62afaf7330c0be3e2adf591fb24a8
Bin <zzbsdh000@outlook.com>
2023-11-19 17:30:04 -0330

    Add files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  console.log(req.body.name, req.body.message);
+  if (req.body.name && req.body.message) {
+    messages.push({
+      text: req.body.message,
+      user: req.body.name,
+      added: new Date(),
+    });
+    res.redirect("/");
+  }
+  res.redirect("/new");
 });
 
 module.exports = router;
